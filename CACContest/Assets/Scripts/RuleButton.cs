9f4effc48e09cc3ed2abd6325372420a46044fcb
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-14 12:24:45 +0900

    add; Button, SceneChange

--- CACContest/Assets/Scripts/RuleButton.cs
+++ CACContest/Assets/Scripts/RuleButton.cs
@@ -1,26 +1,32 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class CustomButton : MonoBehaviour,
+public class RuleButton : MonoBehaviour,
     IPointerClickHandler,
     IPointerDownHandler,
     IPointerUpHandler
 {
+    public System.Action onClickCallback;
+
+    [SerializeField] private CanvasGroup canvasGroup;
     // ボタンをクリックした時
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Button Clicked");
+        onClickCallback?.Invoke();
     }
 
     // ボタンを押している間
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Button Pressed");
+        transform.DOScale(0.95f, 0.24f).SetEase(Ease.OutCubic);
+        canvasGroup.DOFade(0.8f, 0.24f).SetEase(Ease.OutCubic); 
     }
 
     // ボタンを離したとき
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Button Released");
+        transform.DOScale(1f, 0.24f).SetEase(Ease.OutCubic);  
+        canvasGroup.DOFade(1f, 0.24f).SetEase(Ease.OutCubic);
     }
 }
