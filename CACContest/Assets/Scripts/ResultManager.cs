a7f1d900c45504fd00dbdea87f1410e07e4e2c8e
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-12-16 09:05:04 +0900

    Add; UIを強化

--- CACContest/Assets/Scripts/ResultManager.cs
+++ CACContest/Assets/Scripts/ResultManager.cs
@@ -1,35 +1,21 @@
 using UnityEngine;
 using TMPro;
-using System.Collections;
 
 public class ResultManager : MonoBehaviour
 {
     public Apple appleData;
     public TextMeshProUGUI resultText;
+    public TextMeshProUGUI resultPointText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Color32 color = resultText.color;
-        color.a = 0;
-        resultText.color = color;
-        resultText.text = "Result\n" + appleData.result.ToString();
-        StartCoroutine(FadeInText());
+        resultText.text = "Result";
+        resultPointText.text = appleData.score.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    private IEnumerator FadeInText()
-    {
-        Color color = resultText.color;
-        while (color.a < 1.0f)
-        {
-            // 1秒でフェードイン
-            color.a += Time.deltaTime;
-            resultText.color = color;
-            yield return null;
-        }
     }
 }
