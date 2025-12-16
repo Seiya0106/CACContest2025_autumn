143d18c0383f087c147ced0d01fae4f1aa7cf47e
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-12-16 16:35:37 +0900

    revert

--- CACContest/Assets/Scripts/ResultManager.cs
+++ CACContest/Assets/Scripts/ResultManager.cs
@@ -1,23 +1,14 @@
 using UnityEngine;
 using TMPro;
-using unityroom.Api;
 
 public class ResultManager : MonoBehaviour
 {
     public Apple appleData;
     public TextMeshProUGUI resultText;
     public TextMeshProUGUI resultPointText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         resultText.text = "Result";
         resultPointText.text = appleData.score.ToString();
-        UnityroomApiClient.Instance.SendScore(1, appleData.score, ScoreboardWriteMode.HighScoreDesc);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
